29958b92c9f7267ec7c3194c387d3df8106841c7
xuzhy <3085515593@qq.com>
2023-11-20 15:17:27 +0800

    合并

--- README.md
+++ README.md
@@ -5,13 +5,14 @@ pip install -r requirements.txt
 
 ## 运行方法
 
-1. 运行
+1. 虚拟机运行 opengauss 数据库
+2. 运行
     ```
     python manage.py runserver 0.0.0.0:8000
     ```
 2. 同一局域网下访问：主机 IP + 端口，如 172.26.xx.xx:8000
 
-运行成功可以看到 Hello World!
+
 
 
 ## Notice!
--- news/urls.py
+++ news/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),                # 默认登录页面
+    path('', views.index, name='index'),                # 默认登录页面
     path('register', views.register, name='register'),  # 注册
     path('login', views.login, name='login'),           # 登录
     path('index', views.index, name='index'),           # 主页
     path('logout', views.logout, name='logout'),        # 登出
+    
 ]
