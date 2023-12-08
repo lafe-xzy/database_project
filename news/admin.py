1fbb574c56f6239c6515ca43a8495e56562e3250
xuzhy <3085515593@qq.com>
2023-12-08 09:28:28 +0800

    添加管理功能

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+admin.site.register(Campus)
+admin.site.register(Cafeteria)
+admin.site.register(ServedTime)
+admin.site.register(Dish)
+admin.site.register(Comments)
